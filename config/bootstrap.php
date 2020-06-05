31132efb7d6bfe8a4b137dd7d8282c886c5cd8db
Bartek Niewinski <bniewinski@genesis.com.mt>
2020-06-05 14:58:54 +0200

    Fix typo in env var + enable xdebug on dev Dockerfile

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -12,10 +12,8 @@ if (is_array($env = @include dirname(__DIR__) . '/.env.local.php') && (!isset($e
     foreach ($env as $k => $v) {
         $_ENV[$k] = $_ENV[$k] ?? (isset($_SERVER[$k]) && 0 !== strpos($k, 'HTTP_') ? $_SERVER[$k] : $v);
     }
-} elseif (!class_exists(Dotenv::class)) {
-    throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
-} else {
-    // load all the .env files
+}
+if (($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) !== 'prod') {
     (new Dotenv(false))->loadEnv(dirname(__DIR__) . '/.env');
 }
 
